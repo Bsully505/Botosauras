59f264348ac2ca83858c7631a337626ff105af45
tjcj211 <timcarta@hotmail.com>
2021-11-01 12:54:56 -0400

    Removed webhook. Don't put these in public repos. Needed to recreate the webhook because it was compromised.

--- slackbot_test.py
+++ slackbot_test.py
@@ -2,7 +2,7 @@ from slackbot import SlackBot
 import requests
 
 # This is the webhook that allows us to post to #general in SER300 Slack
-URL = "https://hooks.slack.com/services/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"
+URL = "[WEBHOOK]"
 
 # Initialize the slackbot. This is the object that we will be able to use to process the requests and get the responses
 sb = SlackBot() 
