dba8c3dcb4093f34683c828acf83a6aac37ee775
tjcj211 <timcarta@hotmail.com>
2021-11-03 10:05:40 -0400

    Added some more outlining to equipment and spells

--- playerDetails/Equipment.py
+++ playerDetails/Equipment.py
@@ -2,3 +2,10 @@ class Equipment:
     def __init__(self, _player):
         self.player = _player
         self.items = {}
+        self.currency = {
+            "copper": 0,
+            "silver": 0,
+            "electrum": 0,
+            "gold": 0,
+            "platinum": 0
+        }
