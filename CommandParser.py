850624a89fd32e43db85b4ba7a5228d563cabf49
Bsully505 <Bsully505@yahoo.com>
2021-11-06 18:46:32 -0400

    added commands for roll and damage

--- CommandParser.py
+++ CommandParser.py
@@ -1,7 +1,15 @@
 
+from DiceRoller import DiceRoller
+from RollForDamage import RollForDamage
 
 class CommandParser():
     def parse(command):
         #command should be in context of !(key command) (parameters)
-        key = command[0]
-        print(key)
+        key = command.split(' ')[0]
+        switcher={
+            'r':DiceRoller.getRoll('!'+command),
+            'M':RollForDamage.processCommand('!'+command)
+        }
+        return(switcher.get(key, "Invalid entry"))
+
+        
---
+++ RollForDamage.py
@@ -0,0 +1,61 @@
+import random
+
+class RollForDamage:
+
+    #range of 1-20
+    def roll20():
+        return round((random.random()*19)+1)
+    #range of 1-12
+    def roll12():
+        return round((random.random()*11)+1)
+    #range of 1-10
+    def roll10():
+        return round((random.random()*9)+1)
+    #range of 1-8
+    def roll8():
+        return round((random.random()*7)+1)
+    #range of 1-6 aka regular die
+    def roll6():
+        return round((random.random()*5)+1)
+    #range of 1-4
+    def roll4():
+        return round((random.random()*3)+1)
+
+    #checking cases to roll the dice with the corresponing number of sides
+    def pickDiceRoller(self,numOfSides):
+        switcher={
+            20: self.roll20(),
+            12: self.roll12(),
+            10: self.roll10(),
+            8: self.roll8(),
+            6: self.roll6(),
+            4: self.roll4(),
+        }
+        return switcher.get(numOfSides, "Invalid entry")
+
+    def processCommand(self, command):
+        #the format of this command should be !(numberofRoles)M(number of sides on dice)
+        # Removes ! from command and splits at [space-separated] parameters
+        usableCommand = command.split('!')[1]
+
+        print("COMMAND:", usableCommand)
+
+        if usableCommand.find('M') != -1:
+            diceRoll = usableCommand.split('M')
+
+            #determine how many roles will happen
+            numOfRoles= diceRoll[0]
+            print("# ROLLS:", numOfRoles)
+
+            #obtain the type of dice used
+            diceSides = diceRoll[1]
+            print("DICE SIDES:", diceSides)
+
+            sum = 0
+            for i in range(numOfRoles):
+ 	              sum += self.pickDiceRoller(diceSides)
+
+            return sum
+
+        else:
+            return "Invalid entry"
