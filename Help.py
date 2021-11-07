a2a9e990dab90742be9b0c26ad79f61ec1524704
Bsully505 <Bsully505@yahoo.com>
2021-11-07 14:58:21 -0500

    fixed the error that was happening during the demo

--- Help.py
+++ Help.py
@@ -1,10 +1,10 @@
 class Help:
     def getHelp():
         helper = 'Helping with commands \n'
-        rollingHelp = ' Roll a dice: !r (amount of dice rolled)D(number of sides on dice ex 4,8,10,12,20) skill value advantageStatus all seperated by spaces ex !r 1D20 +2 A\n'
+        rollingHelp = ' Roll a dice: !R (amount of dice rolled)D(number of sides on dice ex 4,8,10,12,20) skill value advantageStatus all seperated by spaces ex !r 1D20 +2 A\n'
         helper +=rollingHelp
         AttackCritHelp = 'AttackCrit: !A: no params, returns true or false\n'
         helper+= AttackCritHelp
-        DamageHelp = 'Attack: !M\n'
+        DamageHelp = 'Attack: !M (numberOfRolls)D(SidesofDice)ex !M 1D20 would roll once with the dice being a 20 sided dice\n'
         helper +=DamageHelp
         return helper
