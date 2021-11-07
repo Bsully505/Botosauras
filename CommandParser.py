18af2d4dd569452e328fcb9768bd9ed7e9d5f4ef
Bsully505 <Bsully505@yahoo.com>
2021-11-06 21:17:28 -0400

    lambda functions

--- CommandParser.py
+++ CommandParser.py
@@ -9,10 +9,10 @@ class CommandParser():
         #command should be in context of !(key command) (parameters)
         key = command.split(' ')[0]
         switcher={
-            'r':DiceRoller.getRoll(DiceRoller,'!'+command),
-            'M':RollForDamage.processCommand(RollForDamage,'!'+command),
-            'A':CriticalAttack.Attackroll(CriticalAttack),
-            'H':Help.getHelp()
+            'r':lambda: DiceRoller.getRoll(DiceRoller,'!'+command),
+            'M':lambda: RollForDamage.processCommand(RollForDamage,'!'+command),
+            'A':lambda: CriticalAttack.Attackroll(CriticalAttack),
+            'H':lambda: Help.getHelp()
         }
         return(switcher.get(key, "You did not enter a correct command type !H to get commands"))
 
