47fd2e16b63c32983eedc6242af82d51b1ffde5e
Bsully505 <Bsully505@yahoo.com>
2021-11-17 15:36:32 -0500

    hopefully that was the last syntax error

--- AddPlayer.py
+++ AddPlayer.py
@@ -2,7 +2,7 @@ import requests
  
 class AddPlayer:
     url = 'https://slackevent.herokuapp.com/'
-    def __init__(self,is_DM,User):
+    def AddPlayer(self,is_DM,User):
         res =requests.post(self.url+'PostChar', json= {"type":is_DM,"User":User})
         return res.status_code
         
