8c4de821310318339a108ee1f077fb0085f299ce
christelle flores <57645973+christelleflores@users.noreply.github.com>
2021-11-16 19:20:23 -0500

    read json file

--- playerDetails/AbilityScores/Abilities.py
+++ playerDetails/AbilityScores/Abilities.py
@@ -1,9 +1,23 @@
 from playerDetails.AbilityScores.AbilityScores import AbilityScores
 from playerDetails.AbilityScores.SavingThrows import SavingThrows
-
+import json
 
 class Abilities:
     def __init__(self, _player):
-        self.proficiencyBonus = 0
-        self.abilityScores = AbilityScores(_player)
-        self.savingThrows = SavingThrows(self)
+       self.proficiencyBonus = 0
+       self.abilityScores = AbilityScores(_player)
+       self.savingThrows = SavingThrows(self)
+
+    def parseFile():
+        f = open('characters.json')
+
+        data = json.load(f)
+
+        for i in data['characters']:
+
+            print(i)
+        
+            f.close()
+
+def main(self):
+    self.parseFile()
