3bc36628bd19fdf15b5e2750cb4e0c2f77f39354
Bsully505 <Bsully505@yahoo.com>
2021-11-17 19:10:49 -0500

    testing

--- ListOfPlayers.py
+++ ListOfPlayers.py
@@ -2,12 +2,14 @@
 class Players:
     _instance = None
     global players
+    players = []
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            players=[]
+           
             cls._instance = super(Players, cls).__new__(
                                 cls, *args, **kwargs)
         return cls._instance
+        
     def addPlayer(self,PlayerID):
         self.players.append(PlayerID)
 
