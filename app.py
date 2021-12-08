b543f6f5dcba6579ee515e75443fb0c38c59506c
tjcj211 <timcarta@hotmail.com>
2021-12-07 20:22:51 -0500

    Added self reference to GetPlayer call

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def PrintPlayer():
 
 @App.route('/players/<user>', methods=['GET'])
 def getAbilitiesGivenUser(user):
-    player = TestingChar.GetPlayer(user)
+    player = TestingChar.GetPlayer(TestingChar, user)
     response = Flask.Response()
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.set_data(player.get())
